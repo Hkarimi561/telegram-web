6ae32f971f07e8b1600c07ca3e4801d9c3f78130
Hamidreza Karimi <hkarimi561@gmail.com>
2019-06-30 11:41:26 +0430

    Updates

---
+++ precache-manifest.9fc87c637d323bcc4f057a57e016a752.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "473b380c2a1264e252bc",
+    "url": "/telegram/static/css/main.a7460d94.chunk.css"
+  },
+  {
+    "revision": "473b380c2a1264e252bc",
+    "url": "/telegram/static/js/main.b1dcb957.chunk.js"
+  },
+  {
+    "revision": "753428c465bfa739c14e",
+    "url": "/telegram/static/js/runtime~main.06154a8e.js"
+  },
+  {
+    "revision": "8f727742bba7e8974e8f",
+    "url": "/telegram/static/js/2.7feaa8ef.chunk.js"
+  },
+  {
+    "revision": "949fa29312bce015c85a",
+    "url": "/telegram/static/css/3.33f94faa.chunk.css"
+  },
+  {
+    "revision": "949fa29312bce015c85a",
+    "url": "/telegram/static/js/3.e45201dd.chunk.js"
+  },
+  {
+    "revision": "49ddee2a0f8ed1b6cd5c",
+    "url": "/telegram/static/js/4.64c6cdba.chunk.js"
+  },
+  {
+    "revision": "f0a1235cbf95be83405d",
+    "url": "/telegram/static/css/5.ade432b0.chunk.css"
+  },
+  {
+    "revision": "f0a1235cbf95be83405d",
+    "url": "/telegram/static/js/5.e2946348.chunk.js"
+  },
+  {
+    "revision": "6b7be916ec4387b90e93",
+    "url": "/telegram/static/css/6.82c8279a.chunk.css"
+  },
+  {
+    "revision": "6b7be916ec4387b90e93",
+    "url": "/telegram/static/js/6.8be428cd.chunk.js"
+  },
+  {
+    "revision": "c51398624ae18e93f733",
+    "url": "/telegram/static/js/7.c847218f.chunk.js"
+  },
+  {
+    "revision": "8f34cd20fc1bbebf2dcf",
+    "url": "/telegram/static/js/8.c4a41c8a.chunk.js"
+  },
+  {
+    "revision": "7a71629a5e4f7482b6320b103f1d543c",
+    "url": "/telegram/static/media/Manytabs_2x.7a71629a.png"
+  },
+  {
+    "revision": "fb38f7d6fbf79f207edfa158c10f16a7",
+    "url": "/telegram/static/media/check.fb38f7d6.svg"
+  },
+  {
+    "revision": "5270d71cd78fd282a3736b0e6ae7f048",
+    "url": "/telegram/static/media/General_2x.5270d71c.png"
+  },
+  {
+    "revision": "f01cec9bccc80a678ed0ec04acecade2",
+    "url": "/telegram/static/media/IconsetW_2x.f01cec9b.png"
+  },
+  {
+    "revision": "bd288a1b28b162e1a82654a4b1abbb45",
+    "url": "/telegram/index.html"
+  }
+];
